5ca8e3408afe2dcc6580d4fdb0fdaa2df8a8e004
Petr Babic <xbabic@fi.muni.cz>
2024-12-04 23:08:54 +0100

    use `nice` in run.sh

--- run.sh
+++ run.sh
@@ -18,5 +18,4 @@ echo "#################"
 ## use nice to decrease priority in order to comply with aisa rules
 ## https://www.fi.muni.cz/tech/unix/computation.html.en
 ## especially if you are using multiple cores
-# nice -n 19 ./network
-./network
+nice -n 19 ./network
