82be4004b119e10cf2a446972a4150bfea347848
Petr Babič <72268449+petbab@users.noreply.github.com>
2025-05-09 10:53:48 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-## change this file to your needs
-
-echo "Adding some modules"
-module add gcc-12.2
 
 echo "#################"
 echo "    COMPILING    "
 echo "#################"
 
-## dont forget to use compiler optimizations (e.g. -O3 or -Ofast)
 g++ -std=c++23 -O3 src/main.cpp src/*.h -o network
 
 echo "#################"
 echo "     RUNNING     "
 echo "#################"
 
-## use nice to decrease priority in order to comply with aisa rules
-## https://www.fi.muni.cz/tech/unix/computation.html.en
-## especially if you are using multiple cores
-nice -n 19 ./network
+./network
